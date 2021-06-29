a3ed5126708e8228bd9f904813081f79695313f6
Shannon Bonet <shannonbonet@berkeley.edu>
2021-06-29 16:38:57 -0700

    added enlist

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,32 @@
-import * as React from "react"
-import { StaticQuery, graphql } from 'gatsby'
+import * as React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 import { withStyles } from '@material-ui/core/styles';
 import { styles } from '../styles/customTheme.js';
 
-const TitleAndDescription = ({classes, data}) => {
+const TitleAndDescription = ({ classes, data }) => {
+  // to make sitewide edits, go to gatsby-config -> siteMetaData
+  const { title } = data.site.siteMetadata;
+  const { description } = data.site.siteMetadata;
 
-    //to make sitewide edits, go to gatsby-config -> siteMetaData
-    const title = data.site.siteMetadata.title; 
-    const description = data.site.siteMetadata.description; 
-  
-    return (
-      <div className={classes.header}> 
-        <h1> {title} </h1>
-        <p> {description} </p>
-      </div>
-    )
-  }
+  return (
+    <div className={classes.header}>
+      <h1>
+        {' '}
+        {title}
+        {' '}
+      </h1>
+      <p>
+        {' '}
+        {description}
+        {' '}
+      </p>
+    </div>
+  );
+};
 
-  const Header = ({classes}) => {
-    return (
-      <StaticQuery 
-        query={graphql`
+const Header = ({ classes }) => (
+  <StaticQuery
+    query={graphql`
           query {
             site {
               siteMetadata {
@@ -30,9 +36,8 @@ const TitleAndDescription = ({classes, data}) => {
             }
           }
         `}
-        render={data => <TitleAndDescription classes={classes} data={data} /> } 
-      />
-    ) 
-  }
+    render={(data) => <TitleAndDescription classes={classes} data={data} />}
+  />
+);
 
-  export default withStyles(styles)(Header)
+export default withStyles(styles)(Header);
