5be3318eb26121feb10b1b569f9a8d75ff0034bf
Shannon Bonet <shannonbonet@berkeley.edu>
2021-07-17 19:05:41 -0700

    tested build

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-theme-material-ui',
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp', // Needed for dynamic images
