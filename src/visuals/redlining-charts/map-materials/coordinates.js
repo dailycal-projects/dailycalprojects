7575b3aaba162011ca23b931b773bdcb3c86a1c3
cameronfozi <78174406+cameronfozi@users.noreply.github.com>
2021-07-19 19:13:48 -0700

    fix map ZIP codes

--- src/visuals/redlining-charts/map-materials/coordinates.js
+++ src/visuals/redlining-charts/map-materials/coordinates.js
@@ -43,7 +43,7 @@ export const data = {
       ct: 6001422700, coordinates: [37.8679125, -122.2495401], hbp: 98.79, cespr: '25-30%', color: '#E2565F', ZIP: 94704, pbp: 19.18,
     },
     {
-      ct: 6001421900, coordinates: [37.8794799, -122.2896443], hbp: 5.11, cespr: '25-30%', color: 'green', ZIP: 94702, pbp: 54.59,
+      ct: 6001421900, coordinates: [37.8794799, -122.2896443], hbp: 5.11, cespr: '25-30%', color: '#008002', ZIP: 94702, pbp: 54.59,
     },
     {
       ct: 6001422800, coordinates: [37.8667875, -122.259979], hbp: 99.67, cespr: '25-30%', color: '#E2565F', ZIP: 94704, pbp: 30.67,
@@ -70,7 +70,7 @@ export const data = {
       ct: 6001423601, coordinates: [37.8588968, -122.2587175], hbp: 57.15, cespr: '10-15%', color: '#F28147', ZIP: 94705, pbp: 22.43,
     },
     {
-      ct: 6001421800, coordinates: [37.8802854, -122.2782006], hbp: 17.04, cespr: '5-10%', color: 'green', ZIP: 94703, pbp: 10.93,
+      ct: 6001421800, coordinates: [37.8802854, -122.2782006], hbp: 17.04, cespr: '5-10%', color: '#008002', ZIP: 94703, pbp: 10.93,
     },
     {
       ct: 6001421700, coordinates: [37.8819398, -122.2701843], hbp: 42.68, cespr: '5-10%', color: '#F28147', ZIP: 94709, pbp: 14.11,
@@ -79,19 +79,34 @@ export const data = {
       ct: 6001423700, coordinates: [37.8618413, -122.248923], hbp: 64.67, cespr: '5-10%', color: '#F28147', ZIP: 94705, pbp: 17.25,
     },
     {
-      ct: 6001421400, coordinates: [37.8883409, -122.270922], hbp: 15.72, cespr: '5-10%', color: 'green', ZIP: 94707, pbp: 4.39,
+      ct: 6001421400, coordinates: [37.8883409, -122.270922], hbp: 15.72, cespr: '5-10%', color: '#008002', ZIP: 94707, pbp: 4.39,
     },
     {
-      ct: 6001423800, coordinates: [37.8547587, -122.2441385], hbp: 19.19, cespr: '1-5% (lowest scores)', color: 'green', ZIP: 94705, pbp: 18.2,
+      ct: 6001423800, coordinates: [37.8547587, -122.2441385], hbp: 19.19, cespr: '1-5% (lowest scores)', color: '#008002', ZIP: 94705, pbp: 18.2,
     },
     {
-      ct: 6013391000, coordinates: [37.90719, -122.284319], hbp: 21.34, cespr: '1-5% (lowest scores)', color: 'green', ZIP: 94707, pbp: 15.78,
+      ct: 6013391000, coordinates: [37.90719, -122.284319], hbp: 21.34, cespr: '1-5% (lowest scores)', color: '#008002', ZIP: 94707, pbp: 15.78,
     },
     {
-      ct: 6001400100, coordinates: [37.8675947, -122.2319033], hbp: 12.91, cespr: '1-5% (lowest scores)', color: 'green', ZIP: 94704, pbp: 32.27,
+      ct: 6001400100, coordinates: [37.8675947, -122.2319033], hbp: 12.91, cespr: '1-5% (lowest scores)', color: '#008002', ZIP: 94704, pbp: 32.27,
     },
     {
-      ct: 6001421200, coordinates: [37.8975481, -122.2752271], hbp: 7.39, cespr: '1-5% (lowest scores)', color: 'green', ZIP: 94707, pbp: 8.94,
+      ct: 6001421200, coordinates: [37.8975481, -122.2752271], hbp: 7.39, cespr: '1-5% (lowest scores)', color: '#008002', ZIP: 94707, pbp: 8.94,
+    },
+    {
+      ct: 6001421500, coordinates: [37.8920346, -122.2590314], hbp: 2.34, cespr: '1-5% (lowest scores)', color: '#008002', ZIP: 94708, pbp: 1.36,
+    },
+    {
+      ct: 6001421300, coordinates: [37.8911093, -122.2813663], hbp: 8.3, cespr: '1-5% (lowest scores)', color: '#008002', ZIP: 94707, pbp: 18.9,
+    },
+    {
+      ct: 6001421100, coordinates: [37.899434, -122.2661928], hbp: 7.73, cespr: '1-5% (lowest scores)', color: '#008002', ZIP: 94708, pbp: 2.66,
+    },
+    {
+      ct: 6013392000, coordinates: [37.9094178, -122.2748794], hbp: 15.28, cespr: '1-5% (lowest scores)', color: '#008002', ZIP: 94708, pbp: 5.92,
+    },
+    {
+      ct: 6001421600, coordinates: [37.8839567, -122.2559829], hbp: 12.91, cespr: '1-5% (lowest scores)', color: '#008002', ZIP: 94708, pbp: 15.66,
     },
   ],
   minLat: 37.8503526,
