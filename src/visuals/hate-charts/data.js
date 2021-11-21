7b3a1d47d0db499a0f691674565cbc8052ff332e
Michelle Li <michelleli@Michelles-MacBook-Air.local>
2021-11-21 13:58:55 -0800

    fixing data syntax

--- src/visuals/hate-charts/data.js
+++ src/visuals/hate-charts/data.js
@@ -1,3 +1,136 @@
+export const alldata = [
+  {
+    name: '1995',
+    city_num: 7,
+    campus_num: 0,
+  },
+  {
+    name: '1996',
+    city_num: 3,
+    campus_num: 3,
+  },
+  {
+    name: '1997',
+    city_num: 5,
+    campus_num: 0,
+  },
+  {
+    name: '1998',
+    city_num: 5,
+    campus_num: 1,
+  },
+  {
+    name: '1999',
+    city_num: 6,
+    campus_num: 0,
+  },
+  {
+    name: '2000',
+    city_num: 9,
+    campus_num: 0,
+  },
+  {
+    name: '2001',
+    city_num: 11,
+    campus_num: 0,
+  },
+  {
+    name: '2002',
+    city_num: 28,
+    campus_num: 0,
+  },
+  {
+    name: '2003',
+    city_num: 27,
+    campus_num: 2,
+  },
+  {
+    name: '2004',
+    city_num: 9,
+    campus_num: 2,
+  },
+  {
+    name: '2005',
+    city_num: 8,
+    campus_num: 1,
+  },
+  {
+    name: '2006',
+    city_num: 10,
+    campus_num: 2,
+  },
+  {
+    name: '2007',
+    city_num: 3,
+    campus_num: 3,
+  },
+  {
+    name: '2008',
+    city_num: 0,
+    campus_num: 5,
+  },
+  {
+    name: '2009',
+    city_num: 1,
+    campus_num: 3,
+  },
+  {
+    name: '2010',
+    city_num: 1,
+    campus_num: 15,
+  },
+  {
+    name: '2011',
+    city_num: 4,
+    campus_num: 0,
+  },
+  {
+    name: '2012',
+    city_num: 8,
+    campus_num: 5,
+  },
+  {
+    name: '2013',
+    city_num: 13,
+    campus_num: 7,
+  },
+  {
+    name: '2014',
+    city_num: 3,
+    campus_num: 8,
+  },
+  {
+    name: '2015',
+    city_num: 4,
+    campus_num: 2,
+  },
+  {
+    name: '2016',
+    city_num: 13,
+    campus_num: 4,
+  },
+  {
+    name: '2017',
+    city_num: 23,
+    campus_num: 5,
+  },
+  {
+    name: '2018',
+    city_num: 17,
+    campus_num: 0,
+  },
+  {
+    name: '2019',
+    city_num: 8,
+    campus_num: 0,
+  },
+  {
+    name: '2020',
+    city_num: 7,
+    campus_num: 0,
+  },
+];
+
 export const data1 = [
   {
     name: 'Anti-Gender',
