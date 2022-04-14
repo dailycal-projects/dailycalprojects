afa8ceafe0ef1aecfcacacc1e459c99075e67770
Cameron Fozi <cameronfozi@berkeley.edu>
2022-04-14 14:53:15 -0700

    make visuals folder and migrate code

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,6 +21,7 @@ import GymVault from '../visuals/gym-pac-12-visuals/gymVault';
 import GymBars from '../visuals/gym-pac-12-visuals/gymBars';
 import GymBeam from '../visuals/gym-pac-12-visuals/gymBeam';
 import GymFloor from '../visuals/gym-pac-12-visuals/gymFloor';
+import Timeline1921 from '../visuals/1921-visuals/1921timeline';
 
 /*
 To avoid using exact paths in MDX files, import your components here
@@ -58,6 +59,7 @@ const shortcodes = {
   GymBars,
   GymBeam,
   GymFloor,
+  Timeline1921,
 };
 
 export default function Layout({ children }) {
---
+++ src/visuals/1921-visuals/1921Timeline.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Timeline from '@material-ui/lab/Timeline';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+
+export default function Timeline1921() {
+  return (
+    <Timeline position="alternate">
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <b> 2 October 2021 </b>
+          <br />
+          <em> 1921 Walnut St is built by the overlords. </em>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Code</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+        </TimelineSeparator>
+        <TimelineContent>Sleep</TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  );
+}
