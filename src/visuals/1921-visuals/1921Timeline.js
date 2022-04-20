346ff418a16598ee9e7ebaba116ced0ad77c8ba6
Cameron Fozi <cameronfozi@berkeley.edu>
2022-04-19 17:57:57 -0700

    redesign timeline

--- src/visuals/1921-visuals/1921Timeline.js
+++ src/visuals/1921-visuals/1921Timeline.js
@@ -5,34 +5,206 @@ import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
 import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
 
-export default function Timeline1921() {
-  return (
-    <Timeline position="alternate">
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <b> 2 October 2021 </b>
-          <br />
-          <em> 1921 Walnut St is built by the overlords. </em>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Code</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-        </TimelineSeparator>
-        <TimelineContent>Sleep</TimelineContent>
-      </TimelineItem>
-    </Timeline>
-  );
-}
+const Timeline1921 = () => (
+  <Timeline position="left">
+    <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+        <b> January 2020 </b>
+        <br />
+
+        <a href="https://regents.universityofcalifornia.edu/regmeet/jan20/f12.pdf" target="_blank" rel="noreferrer">
+          <em> A memo to the Regents details a donor’s offer to pay for the design, development and construction of new student housing. </em>
+        </a>
+
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <p> The new development was to be built on five University-owned parcels of the six parcels composing a city block just west of campus. The only parcel on the block the UC did not own was 1921 Walnut St. </p>
+        <br />
+      </TimelineContent>
+    </TimelineItem>
+
+    <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+        <b> Monday, June 29, 2020 </b>
+        <br />
+
+        <a href="https://www.dailycal.org/2020/07/01/canary-in-the-coal-mine-berkeley-residents-protest-for-affordable-housing/" target="_blank" rel="noreferrer">
+          <em> 'I have a lot of history here' </em>
+        </a>
+
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <p> About 25 gather to protest the university&apos;s plan to purchase 1921 Walnut St. </p>
+        <p> A resident since 1989, Davone Riddick said, “This apartment really means a lot to me because I grew up here, and I have a lot of history here.​​” </p>
+        <p> Backing up the tenants were members of the Berkeley Tenants&apos; Union and the Eviction Defense Center. Representatives from Mayor Jesse Arreguín&apos;s office, as well as the office of City Councilmember Kate Harrison, were there. </p>
+        <br />
+
+      </TimelineContent>
+    </TimelineItem>
+
+    <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+        <b> January 2020 </b>
+        <br />
+
+        <em> A memo to the Regents details a donor&apos;s offer to pay for the design, development and construction of new student housing. </em>
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <br />
+        <p> The new development was to be built on five University-owned parcels of the six parcels composing a city block just west of campus. The only parcel on the block the UC did not own was 1921 Walnut St. </p>
+        <br />
+
+      </TimelineContent>
+    </TimelineItem>
+
+    <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+        <b> January 2020 </b>
+        <br />
+
+        <em> A memo to the Regents details a donor&apos;s offer to pay for the design, development and construction of new student housing. </em>
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <br />
+        <p> The new development was to be built on five University-owned parcels of the six parcels composing a city block just west of campus. The only parcel on the block the UC did not own was 1921 Walnut St. </p>
+        <br />
+
+      </TimelineContent>
+    </TimelineItem>
+
+    <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+        <b> January 2020 </b>
+        <br />
+
+        <em> A memo to the Regents details a donor’s offer to pay for the design, development and construction of new student housing. </em>
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <br />
+        <p> The new development was to be built on five University-owned parcels of the six parcels composing a city block just west of campus. The only parcel on the block the UC did not own was 1921 Walnut St. </p>
+
+        <p> The only parcel on the block the UC did not own was 1921 Walnut St. </p>
+        <br />
+
+      </TimelineContent>
+    </TimelineItem>
+
+    <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+        <b> January 2020 </b>
+        <br />
+
+        <em> A memo to the Regents details a donor’s offer to pay for the design, development and construction of new student housing. </em>
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <br />
+        <p> The new development was to be built on five University-owned parcels of the six parcels composing a city block just west of campus. The only parcel on the block the UC did not own was 1921 Walnut St. </p>
+        <br />
+      </TimelineContent>
+    </TimelineItem>
+
+    <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+        <b> January 2020 </b>
+        <br />
+
+        <em> A memo to the Regents details a donor’s offer to pay for the design, development and construction of new student housing. </em>
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <br />
+        <p> The new development was to be built on five University-owned parcels of the six parcels composing a city block just west of campus. The only parcel on the block the UC did not own was 1921 Walnut St. </p>
+        <br />
+      </TimelineContent>
+    </TimelineItem>
+
+    <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+        <b> January 2020 </b>
+        <br />
+
+        <em> A memo to the Regents details a donor’s offer to pay for the design, development and construction of new student housing. </em>
+        <br />
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <br />
+        <p> The new development was to be built on five University-owned parcels of the six parcels composing a city block just west of campus. The only parcel on the block the UC did not own was 1921 Walnut St. </p>
+        <br />
+      </TimelineContent>
+    </TimelineItem>
+
+    <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+        <b> January 2020 </b>
+        <br />
+
+        <em> A memo to the Regents details a donor’s offer to pay for the design, development and construction of new student housing. </em>
+        <br />
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <br />
+        <p> The new development was to be built on five University-owned parcels of the six parcels composing a city block just west of campus. The only parcel on the block the UC did not own was 1921 Walnut St. </p>
+        <br />
+      </TimelineContent>
+    </TimelineItem>
+
+    <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+        <b> January 2020 </b>
+        <br />
+
+        <em> A memo to the Regents details a donor’s offer to pay for the design, development and construction of new student housing. </em>
+        <br />
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <br />
+        <p> The new development was to be built on five University-owned parcels of the six parcels composing a city block just west of campus. The only parcel on the block the UC did not own was 1921 Walnut St. </p>
+        <br />
+      </TimelineContent>
+    </TimelineItem>
+
+  </Timeline>
+);
+
+export default Timeline1921;
