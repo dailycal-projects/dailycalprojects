c48dbe9aae2ed07e26968bb389f337db4c4c0a57
Cameron Fozi <cameronfozi@berkeley.edu>
2022-04-28 06:09:39 -0700

    clean code, copy edit, fact check 1 thing

--- src/visuals/1921-visuals/1921Timeline.js
+++ src/visuals/1921-visuals/1921Timeline.js
@@ -9,22 +9,46 @@ import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
 
 const Timeline1921 = () => (
   <Timeline>
+
     <TimelineItem>
       <TimelineOppositeContent color="text.secondary">
-        <b> Monday, June 29, 2020 </b>
+        <b> January 2020 </b>
         <br />
-
-        <a href="https://www.dailycal.org/2020/07/01/canary-in-the-coal-mine-berkeley-residents-protest-for-affordable-housing/" target="_blank" rel="noreferrer">
-          <em> “I have a lot of history here.” </em>
+        <a href="https://regents.universityofcalifornia.edu/regmeet/jan20/f12.pdf" target="_blank" rel="noreferrer">
+          <em> A memo to the Regents details a donor's offer to finance UC Berkeley's Gateway Project. </em>
         </a>
-
       </TimelineOppositeContent>
       <TimelineSeparator>
         <TimelineDot />
         <TimelineConnector />
       </TimelineSeparator>
       <TimelineContent>
-        {/* <br /> */}
+        <p>
+          Anonymously deemed as “among the University of California's most generous philanthropic partners,” a donor offers to pay for the design, development and construction of an existing plan for a student housing project.
+        </p>
+        <p>
+          The Gateway was to be built on five University-owned parcels of the six parcels composing a city block just west of campus.
+        </p>
+        <p>
+          The only parcel on the block the UC did not own was 1921 Walnut St.
+        </p>
+        <br />
+      </TimelineContent>
+    </TimelineItem>
+
+    <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+        <b> Monday, June 29, 2020 </b>
+        <br />
+        <a href="https://www.dailycal.org/2020/07/01/canary-in-the-coal-mine-berkeley-residents-protest-for-affordable-housing/" target="_blank" rel="noreferrer">
+          <em> “I have a lot of history here.” </em>
+        </a>
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
         <p>
           About 25 gather to protest the university's plan to purchase 1921 Walnut St.
         </p>
@@ -42,9 +66,7 @@ const Timeline1921 = () => (
       <TimelineOppositeContent color="text.secondary">
         <b> July 2020 </b>
         <br />
-
         <em> The university purchases 1921 Walnut St. </em>
-
       </TimelineOppositeContent>
       <TimelineSeparator>
         <TimelineDot />
@@ -59,7 +81,6 @@ const Timeline1921 = () => (
       <TimelineOppositeContent color="text.secondary">
         <b> Monday August 3, 2020 </b>
         <br />
-
         <a href="https://www.dailycal.org/2020/08/03/were-not-going-to-stop-1921-walnut-st-tenants-protest-uc-acquisition/" target="_blank" rel="noreferrer">
           <em> “We're not going to stop until we get UC to listen to us.” </em>
         </a>
@@ -69,7 +90,6 @@ const Timeline1921 = () => (
         <TimelineConnector />
       </TimelineSeparator>
       <TimelineContent>
-        {/* <br /> */}
         <p>
           Tenants and supporters protest outside the UC Office of the President in Oakland.
         </p>
@@ -85,7 +105,6 @@ const Timeline1921 = () => (
       <TimelineOppositeContent color="text.secondary">
         <b> Tuesday, January 26, 2021 </b>
         <br />
-
         <em>
           1921 Walnut St. residents are told they must vacate.
         </em>
@@ -95,7 +114,6 @@ const Timeline1921 = () => (
         <TimelineConnector />
       </TimelineSeparator>
       <TimelineContent>
-        {/* <br /> */}
         <p> UC Berkeley officials inform residents they must move out of the building, offering a monetary incentive of $54,000 or more, in preparation for its demolition. According to campus spokesperson Dan Mogulof, relocation packages “easily reach six figures.” </p>
         <br />
       </TimelineContent>
@@ -105,7 +123,6 @@ const Timeline1921 = () => (
       <TimelineOppositeContent color="text.secondary">
         <b> Monday, February 22, 2021 </b>
         <br />
-
         <a href="https://news.berkeley.edu/2021/02/22/an-update-from-chancellor-christ-on-two-uc-berkeley-student-housing-projects/" target="_blank" rel="noreferrer">
           <em> A place for transfer students </em>
         </a>
@@ -115,7 +132,6 @@ const Timeline1921 = () => (
         <TimelineConnector />
       </TimelineSeparator>
       <TimelineContent>
-        {/* <br /> */}
         <p>
           In an email to all UC Berkeley students, Chancellor Carol Christ details plans for Anchor House, a student housing project aimed at housing and supporting transfer students.
         </p>
@@ -136,7 +152,6 @@ const Timeline1921 = () => (
       <TimelineOppositeContent color="text.secondary">
         <b> Wednesday, March 17, 2021 </b>
         <br />
-
         <a href="https://www.dailycal.org/2021/03/18/we-need-to-stand-together-as-tenants-protesters-oppose-1921-walnut-st-eviction/" target="_blank" rel="noreferrer">
           <em> “We need to stand together as tenants.” </em>
         </a>
@@ -146,7 +161,6 @@ const Timeline1921 = () => (
         <TimelineConnector />
       </TimelineSeparator>
       <TimelineContent>
-        {/* <br /> */}
         <p>
           About 40 community members gather outside 1921 Walnut St. to protest campus's plan to evict tenants to build Anchor House.
         </p>
@@ -164,7 +178,6 @@ const Timeline1921 = () => (
       <TimelineOppositeContent color="text.secondary">
         <b> Saturday, April 24, 2021 </b>
         <br />
-
         <a href="https://www.dailycal.org/2021/04/27/strongly-objecting-community-members-march-against-uc-berkeley-development-plans-at-peoples-park-1921-walnut-st/" target="_blank" rel="noreferrer">
           <em> About 100 protest development plans at 1921 Walnut St., People's Park. </em>
         </a>
@@ -174,7 +187,6 @@ const Timeline1921 = () => (
         <TimelineConnector />
       </TimelineSeparator>
       <TimelineContent>
-        {/* <br /> */}
         <p>
           Community members march against campus plans to redevelop 1921 Walnut St. and People's Park.
         </p>
@@ -192,7 +204,6 @@ const Timeline1921 = () => (
       <TimelineOppositeContent color="text.secondary">
         <b> Wednesday, July 21, 2021 </b>
         <br />
-
         <a href="https://www.dailycal.org/2021/07/23/here-because-i-am-mad-community-activists-protest-uc-board-of-regents/" target="_blank" rel="noreferrer">
           <em> “I'm here because I am mad.” </em>
         </a>
@@ -202,7 +213,6 @@ const Timeline1921 = () => (
         <TimelineConnector />
       </TimelineSeparator>
       <TimelineContent>
-        {/* <br /> */}
         <p>
           The Coalition for a Truly Public UC gathered in downtown Oakland at 8 a.m. Wednesday to protest the UC Board of Regents, including campus’s plans to redevelop People’s Park and 1921 Walnut St.
         </p>
@@ -217,7 +227,6 @@ const Timeline1921 = () => (
       <TimelineOppositeContent color="text.secondary">
         <b> Tuesday, July 27, 2021 </b>
         <br />
-
         <a href="https://www.jessearreguin.com/press-releases/2021/7/14/city-council-approves-historic-agreement-with-university-of-california-berkeley" target="_blank" rel="noreferrer">
           <em> Campus and city reach an $82.6M settlement. </em>
         </a>
@@ -227,43 +236,12 @@ const Timeline1921 = () => (
         <TimelineConnector />
       </TimelineSeparator>
       <TimelineContent>
-        {/* <br /> */}
-        <p>
-          The $82.6 million settlement includes a $920,000 payment to recognize campus's planned demolition of the eight rent-controlled units at 1921 Walnut St.
-        </p>
-        <p>
-          As part of the agreement, the city cannot legally challenge Anchor House, among other university projects.
-        </p>
-        <p>
-          The settlement also detailed that campus would explore relocating the building but only if the relocation does not increase the time needed to complete the project or delay the construction of Anchor House.
-        </p>
-        <p>
-          The settlement was signed by Arreguín.
-        </p>
-        <br />
-      </TimelineContent>
-    </TimelineItem>
-
-    <TimelineItem>
-      <TimelineOppositeContent color="text.secondary">
-        <b> Tuesday, July 27, 2021 </b>
-        <br />
-
-        <a href="https://www.jessearreguin.com/press-releases/2021/7/14/city-council-approves-historic-agreement-with-university-of-california-berkeley" target="_blank" rel="noreferrer">
-          <em> Campus and city reach an $82.6M settlement. </em>
-        </a>
-      </TimelineOppositeContent>
-      <TimelineSeparator>
-        <TimelineDot />
-        <TimelineConnector />
-      </TimelineSeparator>
-      <TimelineContent>
-        {/* <br /> */}
         <p>
           The
           {' '}
-          <a href="https://www.cityofberkeley.info/uploadedFiles/City_Manager/Level_1_-_Miscellaneous/FULLY%20EXECUTED%20SETTLEMENT%20AGREEMENT.pdf">$82.6 million settlement</a>
+          <a href="https://www.cityofberkeley.info/uploadedFiles/City_Manager/Level_1_-_Miscellaneous/FULLY%20EXECUTED%20SETTLEMENT%20AGREEMENT.pdf" target="_blank" rel="noreferrer">$82.6 million settlement</a>
           {' '}
+
           includes a $920,000 payment to recognize campus's planned demolition of the eight rent-controlled units at 1921 Walnut St.
         </p>
         <p>
@@ -283,7 +261,6 @@ const Timeline1921 = () => (
       <TimelineOppositeContent color="text.secondary">
         <b> Monday, August 2, 2021 </b>
         <br />
-
         <a href="https://www.berkeleyside.org/2021/08/02/op-ed-new-campus-agreement-is-an-80-million-win-for-the-city" target="_blank" rel="noreferrer">
           <em> Berkeleyside publishes an opinion article by Arreguín and Councilmember Rigel Robinson. </em>
         </a>
@@ -305,7 +282,6 @@ const Timeline1921 = () => (
       <TimelineOppositeContent color="text.secondary">
         <b> September 2021 </b>
         <br />
-
         <a href="https://www.dailycal.org/2021/09/27/last-tenants-of-1921-walnut-st-agree-to-vacate-affordable-units/" target="_blank" rel="noreferrer">
           <em> Tenants accept relocation packages and agree to leave. </em>
         </a>
@@ -315,7 +291,6 @@ const Timeline1921 = () => (
         <TimelineConnector />
       </TimelineSeparator>
       <TimelineContent>
-        {/* <br /> */}
         <p>
           The last of the 1921 Walnut St. tenants agree to vacate their rentals with relocation packages that could reach six figures.
         </p>
@@ -330,7 +305,6 @@ const Timeline1921 = () => (
       <TimelineOppositeContent color="text.secondary">
         <b> Thursday, December 16, 2021 </b>
         <br />
-
         <a href="https://www.berkeleyside.org/2021/12/16/student-housing-uc-berkeley-walnut-street-demolition" target="_blank" rel="noreferrer">
           <em> 1921 Walnut St. is demolished. </em>
         </a>
@@ -340,7 +314,6 @@ const Timeline1921 = () => (
         <TimelineConnector />
       </TimelineSeparator>
       <TimelineContent>
-        {/* <br /> */}
         <p>
           The newly cleared land will makes way for the Helen Diller Anchor House.
         </p>
@@ -352,17 +325,14 @@ const Timeline1921 = () => (
       <TimelineOppositeContent color="text.secondary">
         <b> Monday, January 31, 2022 </b>
         <br />
-
         <a href="https://www.dailycal.org/2022/02/04/level-playing-field-uc-berkeley-breaks-ground-on-anchor-house/" target="_blank" rel="noreferrer">
           <em> UC Berkeley breaks ground on Anchor House. </em>
         </a>
       </TimelineOppositeContent>
       <TimelineSeparator>
         <TimelineDot />
-        {/* <TimelineConnector /> */}
       </TimelineSeparator>
       <TimelineContent>
-        {/* <br /> */}
         <p>
           While the project's construction is fully funded by the Helen Diller Foundation, the revenue from the businesses within the building will help secure scholarships for first-generation and underrepresented students.
         </p>
