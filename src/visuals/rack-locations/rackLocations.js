db8725af7802f0198ec6f31679130d60ab752e54
Cameron Fozi <cameronfozi@berkeley.edu>
2022-06-18 12:52:02 -0700

    pre-written locations done

---
+++ src/visuals/rack-locations/rackLocations.js
@@ -0,0 +1,302 @@
+export const rackLocations = {
+
+    locations: [
+
+            {
+              "Location": "UCPD",
+              "lat": 37.86941,
+              "lon": -122.25851
+            },
+            {
+              "Location": "Cafe Milano",
+              "lat": 37.86865,
+              "lon": -122.25854
+            },
+            {
+                "Location": "YWCA",
+                "lat": 37.86895473518469,
+                "lon": -122.2565902763223
+            },
+            {
+                "Location": "RSF",
+                "lat": 37.86895,
+                "lon": -122.26217
+            },
+            {
+                "Location": "Haas Pavilion",
+                "lat": 37.86944,
+                "lon": -122.26207
+            },
+            {
+                "Location": "Upper Sproul",
+                "lat": 37.86975,
+                "lon": -122.2593
+            },
+            {
+                "Location": "Sather Gate",
+                "lat": 37.8701,
+                "lon": -122.25935
+            },
+            {
+                "Location": "Dwinelle Hall",
+                "lat": 37.8708,
+                "lon": -122.26052
+            },
+            {
+                "Location": "West Wheeler",
+                "lat": 37.87133,
+                "lon": -122.25955
+            },
+            {
+                "Location": "Oxford / Center",
+                "lat": 37.87057,
+                "lon": -122.26574
+            },
+            {
+                "Location": "VLSB",
+                "lat": 37.87118,
+                "lon": -122.26208
+            },
+            {
+                "Location": "Pat Brown's",
+                "lat": 37.87332,
+                "lon": -122.26487
+            },
+            {
+                "Location": "Soda Hall #1",
+                "lat": 37.87554,
+                "lon": -122.25873
+            },
+            {
+                "Location": "Evans Hall Bus Stop",
+                "lat": 37.8734,
+                "lon": -122.25741
+            },
+            {
+                "Location": "Evans Hall West Entrance",
+                "lat": 37.87336682927876,
+                "lon": -122.25800468123705
+            },
+            {
+                "Location": "Pimentel Hall",
+                "lat": 37.87337,
+                "lon": -122.25608
+            },
+            {
+                "Location": "Haas Piedmont",
+                "lat": 37.87221,
+                "lon": -122.25343
+            },
+            {
+                "Location": "Caffe Strada",
+                "lat": 37.86917,
+                "lon": -122.2547
+            },
+            {
+                "Location": "Hearst Gym Bus Stop",
+                "lat": 37.86907,
+                "lon": -122.25691
+            },
+            {
+                "Location": "Norton Hall Unit 3",
+                "lat": 37.86723,
+                "lon": -122.25986
+            },
+            {
+                "Location": "Unit 1",
+                "lat": 37.86782,
+                "lon": -122.25518
+            },
+            {
+                "Location": "Telegraph Commons",
+                "lat": 37.86678,
+                "lon": -122.25912
+            },
+            {
+                "Location": "Walgreens",
+                "lat": 37.86837,
+                "lon": -122.25927
+            },
+            {
+                "Location": "La Val's",
+                "lat": 37.87546,
+                "lon": -122.2605
+            },
+            {
+                "Location": "Cory Hall Bus Stop",
+                "lat": 37.87529,
+                "lon": -122.25797
+            },
+            {
+                "Location": "Foothill Dining Hall",
+                "lat": 37.8754,
+                "lon": -122.25581
+            },
+            {
+                "Location": "North Gate Bus Stop",
+                "lat": 37.87514,
+                "lon": -122.26006
+            },
+            {
+                "Location": "University Hall",
+                "lat": 37.87173,
+                "lon": -122.26669
+            },
+            {
+                "Location": "Shattuck/Vine SW",
+                "lat": 37.88016,
+                "lon": -122.26943
+            },
+            {
+                "Location": "Fedex Shattuck/Allston",
+                "lat": 37.86931,
+                "lon": -122.26767
+            },
+            {
+                "Location": "Yali's Cafe",
+                "lat": 37.87351,
+                "lon": -122.26641
+            },
+            {
+                "Location": "Au Coquelet",
+                "lat": 37.87163,
+                "lon": -122.27051
+            },
+            {
+                "Location": "Peet's (Solano)",
+                "lat": 37.89156,
+                "lon": -122.27924
+            },
+            {
+                "Location": "Starbucks (Solano)",
+                "lat": 37.89148,
+                "lon": -122.28004
+            },
+
+
+            // FIXME
+                // CESAR CHAVEZ #1
+                // CESAR CHAVEZ #2
+                // ESHLEMAN
+                // UNIT 2
+                // WEWORK
+                // SAFEWAY COMMUNITY
+                // SAFEWAY
+                // MONTEREY
+
+            //  there should be 42 total above this point--there are !
+
+
+            {
+                "Location": "I-House",
+                "lat": 37.86964,
+                "lon": -122.25182
+            },
+            {
+                "Location": "Ashby BART",
+                "lat": 37.85289,
+                "lon": -122.26993
+            },
+            {
+                "Location": "Rose Garden Inn",
+                "lat": 37.85996,
+                "lon": -122.25956
+            },
+            {
+                "Location": "Bing Wong Wash",
+                "lat": 37.86316,
+                "lon": -122.25882
+            },
+            {
+                "Location": "The Daily Californian",
+                "lat": 37.87508,
+                "lon": -122.26062
+            },
+            {
+                "Location": "Berkeley City Club",
+                "lat": 37.86748,
+                "lon": -122.26281
+            },
+            {
+                "Location": "BSC Fenwick",
+                "lat": 37.86497,
+                "lon": -122.26005
+            },
+            {
+                "Location": "Clark Kerr Campus",
+                "lat": 37.86374,
+                "lon": -122.24812
+            },
+            {
+                "Location": "Cal Suds",
+                "lat": 37.8617,
+                "lon": -122.2533
+            },
+            {
+                "Location": "Redwood Gardens",
+                "lat": 37.86331,
+                "lon": -122.24717
+            },
+            {
+                "Location": "Peet's (Domingo)",
+                "lat": 37.85871,
+                "lon": -122.24414
+            },
+            {
+                "Location": "Star Grocery",
+                "lat": 37.85484,
+                "lon": -122.24834
+            },
+            {
+                "Location": "Cafe Roma",
+                "lat": 37.85686,
+                "lon": -122.25326
+            },
+            {
+                "Location": "Campus Shared Services",
+                "lat": 37.87338,
+                "lon": -122.30214
+            },
+            {
+                "Location": "Peet's (4th Street)",
+                "lat": 37.87025,
+                "lon": -122.30104
+            },
+            {
+                "Location": "Mechanics Bank",
+                "lat": 37.86909,
+                "lon": -122.30004
+            },
+            {
+                "Location": "Berkeley Amtrak",
+                "lat": 37.86751,
+                "lon": -122.30081
+            },
+            {
+                "Location": "La Quinta Inn",
+                "lat": 37.86814,
+                "lon": -122.29574
+            },
+            {
+                "Location": "Rockridge BART",
+                "lat": 37.84465,
+                "lon": -122.25153
+            },
+
+
+
+            // FIXME
+                // Alcatraz & Adeline
+                // PEETS tlgrph dwight
+                // CREAM on channing
+                // hotel durant
+                // BAKER AND COMMONS
+                // safeway rockridge
+                // safeway shattuck
+                // oakland couthouse
+
+
+
+    ]
+
+}
