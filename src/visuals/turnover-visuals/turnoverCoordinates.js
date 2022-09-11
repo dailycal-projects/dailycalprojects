0fabfe74ef7ce6ec6e0b128b6fc87c11bca00970
Cameron Fozi <cameronfozi@berkeley.edu>
2022-09-10 19:41:15 -0700

    coords 2

--- src/visuals/turnover-visuals/turnoverCoordinates.js
+++ src/visuals/turnover-visuals/turnoverCoordinates.js
@@ -1,3 +1,5 @@
+import { isNonNullChain } from "typescript"
+
 export const turnoverCoordinates = {
     info: [
 
@@ -6,21 +8,112 @@ export const turnoverCoordinates = {
             status: "Closed",
             approximateClosure: "11/2020",
             address: "2996 Telegraph Avenue",
-            coordinates: [37.856733277598124, -122.25983728206509]
-
+            coordinates: [37.856733277598124, -122.25983728206509],
+            note: "Aliberto’s Jr. Mexican Food opened at this address in June 2022",
+            color: "purple"
         },
-Bill's men's shop (-2020? 2021? DC/bside article say 1/2020 but streetview shows open in 1/2021) (2386 telegraph) (37.867315695262135, -122.25867562376372)
-Boileroom (1/2020-3/2020) (2475 telegraph) (37.865601617363396, -122.2582947500843)
-Buffalo exchange (12/2020) (2585 telegraph) (37.86393422218963, -122.25803992191081)
-Daiso (-6/2020) (2369 telegraph) (37.86755490392055, -122.25848213725511)
-Finfine (3/2020) (2556 telegraph) (demolition etc) (37.8641396671781, -122.25894226246976)
-j-town express (-12/2020) (2332 telegraph) (37.8683409325918, -122.25872843169734)
-Krishna copy (between 4/19 and 1/21) (2595 telegraph) (37.8636390617205, -122.25764805497575)
-Sojo ramen (5/2021-1?2?/2022) (2475 telegraph) (37.865601617363396, -122.2582947500843)
-Tako sushi (5/2022) (2379 telegraph) (37.867387025406536, -122.25844145259931)
-Enedina tacqueria (-6/2021??) (3001 telegraph) (37.855895645218915, -122.2593851832884)
-So-so supermarket (-12/2021?) (2333 telegraph) (popup per DC; reconfirm?) (37.86833183653382, -122.25845178143547)
-Pappy's (-3/2022 (2367 telegraph) (37.86774508543698, -122.2582547470417)
+        {
+            name: "Bill's Men's Shop",
+            status: "Closed",
+            approximateClosure: "10/2020",
+            address: "2386 Telegraph Avenue",
+            coordinates: [37.867315695262135, -122.25867562376372],
+            note: "Sold and replaced by Beck's Shoes in 10/2020 due to business owner's retirement.",
+            color: "red"
+        },
+        {
+            name: "Boileroom",
+            status: "Closed",
+            approximateClosure: "4/2020",
+            address: "2475 Telegraph Avenue",
+            coordinates: [37.865601617363396, -122.2582947500843],
+            note: "Closed in support of California shelter-in-place order",
+            color: "red"
+        },
+        {
+            name: "Buffalo Exchange",
+            status: "Closed",
+            approximateClosure: "11/2020",
+            address: "2585 Telegraph Avenue",
+            coordinates: [37.86393422218963, -122.25803992191081],
+            note: null,
+            color: "red"
+        },
+        {
+            name: "Daiso",
+            status: "Closed",
+            approximateClosure: "6/2020",
+            address: "2369 Telegraph Avenue",
+            coordinates: [37.86755490392055, -122.25848213725511],
+            note: null,
+            color: "red"
+        },
+        {
+            name: "Finfine",
+            status: "Closed",
+            approximateClosure: "3/2020",
+            address: "2556 Telegraph Avenue",
+            coordinates: [37.8641396671781, -122.25894226246976],
+            note: "Closed for shelter-in-place order and demolition of The Village mall. Business owners are looking to re-open in Berkeley.",
+            color: "red"
+        },
+        {
+            name: "J-Town Express",
+            status: "Closed",
+            approximateClosure: "12/2020",
+            address: "2332 Telegraph Avenue",
+            coordinates: [37.8683409325918, -122.25872843169734],
+            note: null,
+            color: "red"
+        },
+        {
+            name: "Sojo Ramen",
+            status: "Closed",
+            approximateClosure: "2/2022",
+            address: "2475 Telegraph Avenue",
+            coordinates: [37.865601617363396, -122.2582947500843],
+            note: null,
+            color: "red"
+        },
+        {
+            name: "Tako Sushi",
+            status: "Closed",
+            approximateClosure: "5/2022",
+            address: "2379 Telegraph Avenue",
+            coordinates: [37.867387025406536, -122.25844145259931],
+            note: null,
+            color: "red"
+        },
+        {
+            name: "Enedina Taqueria",
+            status: "Closed",
+            approximateClosure: "6/2021",
+            address: "3001 Telegraph Avenue",
+            coordinates: [37.855895645218915, -122.2593851832884],
+            note: null,
+            color: "red"
+        },
+        {
+            name: "So-so Supermarket",
+            status: "Closed",
+            approximateClosure: "12/2021",
+            address: "2333 Telegraph Avenue",
+            coordinates: [37.86833183653382, -122.25845178143547],
+            note: "Ran as a three-month-long pop up shop between September 2021 and December 2021",
+            color: "red"
+        },
+        {
+            name: "Pappy's",
+            status: "Closed",
+            approximateClosure: "3/2022",
+            address: "2367 Telegraph Avenue",
+            coordinates: [37.86833183653382, 37.86774508543698, -122.2582547470417],
+            note: null,
+            color: "red"
+        },
+        
+        
+        
       
     ],
     minLat: 37.8503526,
