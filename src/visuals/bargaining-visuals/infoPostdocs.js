ea4ca038781ebf07cccca134b41e1faa51a94bf3
sanjana-f <sanjana@Sanjanas-Air-2.fios-router.home>
2022-11-21 14:23:30 -0800

    added appointment article for postdocs

--- src/visuals/bargaining-visuals/infoPostdocs.js
+++ src/visuals/bargaining-visuals/infoPostdocs.js
@@ -1,8 +1,8 @@
-export const lastUpdated = 'Last updated on Nov. 21, 2022 at 11:52 a.m.';
+export const lastUpdated = 'Last updated on Nov. 21, 2022 at 2:22 p.m.';
 
 export const articlesPostdocs = [
 
-  'Recognition',
+  'Recognition', 'Appointments'
 
 ];
 
@@ -28,6 +28,91 @@ const recognitionProposals = [
 
 ];
 
+const appointmentsProposals = [
+
+  {
+    name: 'Proposal | UC to UAW | Aug. 17, 2021',
+    link: 'https://drive.google.com/file/d/10dqw-RuCf56cxYm_iRbOw5wOk56BzLsZ/view?usp=sharing',
+    color: 'secondary',
+  },
+  {
+    name: 'Proposal | UAW to UC | Sept. 9, 2021',
+    link: 'https://drive.google.com/file/d/12X7NjtH8ZTI90r5mQOIp_xllivR6rESk/view?usp=sharing',
+    color: 'secondary',
+  },
+  {
+    name: 'Package | UC to UAW | Dec. 9, 2021',
+    link: 'https://drive.google.com/file/d/1vHzuVRMmQ94bn11Zczkuns_j5vHkYzlr/view?usp=sharing',
+    color: 'secondary',
+  },
+  {
+    name: 'Package | UAW to UC | Dec. 15, 2021',
+    link: 'https://drive.google.com/file/d/15-Gc2SPyHq6MINEmOnSf1lw2ZGZ8pjg0/view?usp=sharing',
+    color: 'secondary',
+  },
+  {
+    name: 'Package | UC to UAW | May 4, 2022',
+    link: 'https://drive.google.com/file/d/1xXrZglJs2qkYrFfUI73ZPsTjTnIbsunH/view?usp=sharing',
+    color: 'secondary',
+  },
+  {
+    name: 'Package | UAW to UC | June 2, 2022',
+    link: 'https://drive.google.com/file/d/1P780SLDTcLI4lRHvqIaFPEtKZz0s-vxx/view?usp=sharing',
+    color: 'secondary',
+  },
+  {
+    name: 'Proposal | UC to UAW | June 9, 2022',
+    link: 'https://drive.google.com/file/d/1cjKQDNvW8KJpLc_8tZ2xYk05VD7U_kSd/view?usp=sharing',
+    color: 'secondary',
+  },
+  {
+    name: 'Package | UC to UAW | June 9, 2022',
+    link: 'https://drive.google.com/file/d/13ZhLo8WbB_JH9NG9dAez46i_812s1RvB/view?usp=sharing',
+    color: 'secondary',
+  },
+  {
+    name: 'Package | UC to UAW | Aug. 25, 2022',
+    link: 'https://drive.google.com/file/d/1RPrzVGzTwGGOB2_HcR9S56Y6Lw28viwd/view?usp=sharing',
+    color: 'secondary',
+  },
+  {
+    name: 'Package | UC to UAW | Oct. 10, 2022',
+    link: 'https://drive.google.com/file/d/1XWuJevYrcD-6rnMjXFJIpGM1IaxyYoq-/view?usp=sharing',
+    color: 'secondary',
+  },
+  {
+    name: 'Package | UAW to UC | Oct. 10, 2022',
+    link: 'https://drive.google.com/file/d/1Mtoar_LrDwNVIAzBLh3pBHGHyezrKROP/view?usp=sharing',
+    color: 'secondary',
+  },
+  {
+    name: 'Package | UC to UAW | Nov. 11 , 2022',
+    link: 'https://drive.google.com/file/d/17bdKktjZPvIBcc8WLiRJANFHVlt1g_B2/view?usp=share_link',
+    color: 'secondary',
+  },
+  {
+    name: 'Package | UC to UAW | Nov. 13 , 2022',
+    link: 'https://drive.google.com/file/d/1vDnBFZSbohW7EpB2fA47jV7zs8N0E8Lm/view?usp=share_link',
+    color: 'secondary',
+  },
+  {
+    name: 'Package | UAW to UC | Nov. 13, 2022',
+    link: 'https://drive.google.com/file/d/1apk9KZyph3kN_olbYevfQdNiF6u3i7G4/view?usp=share_link',
+    color: 'secondary',
+  },
+  {
+    name: 'Supposal | UC to UAW | Nov. 19, 2022',
+    link: 'https://drive.google.com/file/d/176lSmk0bNAu5jJzJpBDDvxtU8_-qi-s8/view?usp=share_link',
+    color: 'secondary',
+  },
+  {
+    name: 'Supposal | UAW to UC | Nov. 20, 2022',
+    link: 'https://drive.google.com/file/d/1w13LQoVeYRgw1KjATsGszamL21h1b5GF/view?usp=share_link',
+    color: 'secondary',
+  }
+
+]
+
 export const infoPostdocs = {
 
   Recognition: {
@@ -36,5 +121,10 @@ export const infoPostdocs = {
     intro: 'Leave out intros until night has copy edited them all.',
 
   },
+  Appointments: {
 
+    proposals: appointmentsProposals,
+    intro: 'Leave out intros until night has copy edited them all.',
+
+  }
 };
