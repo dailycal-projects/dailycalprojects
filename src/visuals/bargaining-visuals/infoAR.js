cfe540f76f8850a999e80c871af8526c07616f49
“arfamomin” <arfamomin@Arfas-MacBook-Pro.local>
2022-11-21 14:09:36 -0800

    add benefits and retiremnet programs

--- src/visuals/bargaining-visuals/infoAR.js
+++ src/visuals/bargaining-visuals/infoAR.js
@@ -3,6 +3,7 @@ export const lastUpdated = 'Last updated on Nov. 21, 2022 at 11:52 a.m.';
 export const articlesAR = [
 
   'Recognition',
+  'Benefits and retirement programs',
 
 ];
 
@@ -32,6 +33,25 @@ const recognitionProposals = [
 
 ];
 
+const benefitsRetirementPrograms = [
+  {
+    name: 'Proposal | UAW to UC | Aug. 12, 2022',
+    link: 'https://drive.google.com/file/d/1o_X6YadXS16NEn7kzNT53K_TaRZbVaHy/view?usp=sharing',
+    color: 'secondary',
+  },
+
+  {
+    name: 'Package | UAW to UC | Nov. 12, 2022',
+    link: 'https://drive.google.com/file/d/1EpmcAZUJGrXm-A7zO84SolCLyCktiITV/view?usp=share_link',
+    color: 'secondary',
+  },
+{
+    name: 'Package | UC to UAW | Nov. 17, 2022',
+    link: 'https://drive.google.com/file/d/1-RrNTZc-uyQQ4orlPibGZwNRSMRR5we9/view?usp=share_link',
+    color: 'secondary',
+  }
+];
+
 // Instantiate a list of dictionaries for each article, appendix or side letter here.
 
 export const infoAR = {
@@ -39,6 +59,13 @@ export const infoAR = {
   Recognition: {
 
     proposals: recognitionProposals,
+    intro: 'Leave out intros until night has copy edited them all.',
+
+  },
+  
+  "Benefits and retirement programs": {
+
+    proposals: benefitsRetirementPrograms,
     intro: 'Leave out intros until night has copy edited them all.',
 
   },
