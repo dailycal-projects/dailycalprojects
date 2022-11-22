2d265387e1f47dc4cdf2465bd0efa3cd98046973
nsuliman <nsuliman@dailycal.org>
2022-11-22 10:12:19 -0800

    a few more articles

--- src/visuals/bargaining-visuals/infoSR.js
+++ src/visuals/bargaining-visuals/infoSR.js
@@ -1,9 +1,14 @@
-export const lastUpdated = 'Nov. 21, 2022 at 11:52 a.m.';
+export const lastUpdated = 'Nov. 22, 2022 at 8:24 a.m.';
 
 export const articlesSR = [
 
   'Access needs and reasonable accommodations',
   'Anti-bullying and respectful work environment',
+  'Appointment notification',
+  'Appointment security',
+  'Childcare',
+  'Community safety',
+  'Defined contribution and university retirement plan(s)',
 
 ];
 
@@ -28,6 +33,66 @@ const respectfulProposals = [
   },
 ];
 
+const AppointmentnotificationProposals = [
+
+  {
+    name: 'Tentative agreement | Nov. 20, 2022',
+    link: 'https://drive.google.com/file/d/1Fzc-2nvuTUUCLQj2HGvVNtyragrF_UdC/view?usp=share_link',
+    color: 'success',
+  },
+];
+
+const AppointmentsecurityProposals = [
+
+  {
+    name: 'Tentative agreement | Nov. 11, 2022',
+    link: 'https://drive.google.com/file/d/1UT5FwAvdsJkKNh7dZIRk42zfQs3XaQ-o/view?usp=share_link',
+    color: 'success',
+  },
+];
+
+const ChildcareProposals = [
+
+  {
+    name: 'Proposal | UAW to UC | April 22, 2022',
+    link: 'https://drive.google.com/file/d/1TZbqtncHdPYl1x2zjp2mNWnoWkKS-1Lv/view?usp=sharing',
+    color: 'secondary',
+  },
+  {
+    name: 'Proposal | UC to UAW | Oct. 13, 2022',
+    link: 'https://drive.google.com/file/d/16NX19Bic1MKImLUNQS04SOkj2IZ4R7DI/view?usp=sharing',
+    color: 'secondary',
+  },
+  {
+    name: 'Proposal | UC to UAW | Oct. 13, 2022',
+    link: 'https://drive.google.com/file/d/1yTOQQh1s_R3LIW5rvw928UtjSsuiK43O/view?usp=sharing',
+    color: 'secondary',
+  },
+  {
+    name: 'Proposal | UC to UAW | Nov. 16, 2022',
+    link: 'https://drive.google.com/file/d/11A7HmQutdrGPxDSbQJXRmTQrRTZAGa8Q/view?usp=share_link',
+    color: 'secondary',
+  },
+];
+
+const communitySafetyProposals = [
+
+  {
+    name: 'Proposal | Sept. 21, 2022',
+    link: 'https://drive.google.com/file/d/103xbSowGfFVICduoKvravDOZuAVDBHHV/view',
+    color: 'secondary',
+  },
+];
+
+const DefinedcontributionProposals = [
+
+  {
+    name: 'Tentative agreement | Nov. 11, 2022',
+    link: 'https://drive.google.com/file/d/1JuK52i5jNQV6eZAAK22sywjJvHacXT6z/view?usp=share_link',
+    color: 'success',
+  },
+];
+
 export const infoSR = {
 
   'Access needs and reasonable accommodations': {
@@ -42,5 +107,35 @@ export const infoSR = {
     intro: 'Leave out intros until night has copy edited them all.',
 
   },
+  'Appointment notification': {
 
+    proposals: AppointmentnotificationProposals,
+    intro: 'Leave out intros until night has copy edited them all.',
+
+  },
+  'Appointment security': {
+
+    proposals: AppointmentsecurityProposals,
+    intro: 'Leave out intros until night has copy edited them all.',
+
+  },
+  'Childcare': {
+
+    proposals: ChildcareProposals,
+    intro: 'Leave out intros until night has copy edited them all.',
+
+  },
+  'Community Safety': {
+
+    proposals: communitySafetyProposals,
+    intro: 'Leave out intros until night has copy edited them all.',
+
+  },
+  'Defined contribution and university retirement plan(s)':{
+
+    proposals: DefinedcontributionProposals,
+    intro: 'Leave out intros until night has copy edited them all.',
+
+  },
 };
+
