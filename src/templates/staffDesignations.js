2ee179ab84a93985618eaad90fe0498cd9a45472
Cameron Fozi <cameronfozi@berkeley.edu>
2023-11-17 19:16:10 -0800

    staff des

--- src/templates/staffDesignations.js
+++ src/templates/staffDesignations.js
@@ -10,6 +10,10 @@ const seniorStaff = [
   'Ekansh Agrawal',
   'Lydia Sidhom',
   'Cameron Fozi',
+  'Clara Brownstein',
+  'Angela Bi',
+  'Arfa Momin',
+  'Riya Chopra',
 
 ];
 
@@ -17,17 +21,21 @@ export const staffTitles = {
   titles: [
     { name: 'Paloma Torres', title: 'the projects editor' },
     { name: 'Arfa Momin', title: 'the deputy projects editor' },
-    { name: 'Cameron Fozi', title: 'a projects developer and the projects training manager. He was the spring, summer and fall 2022 projects editor' },
-    { name: 'Nibras Suliman', title: 'a projects developer and former deputy projects editor' },
-    { name: 'Isabella Borkovic', title: 'a projects developer and former deputy projects editor' },
-    { name: 'Veronica Roseborough', title: 'a projects developer and former deputy projects editor' },
-    { name: 'Erica Jean', title: 'a projects developer' },
+    { name: 'Lydia Sidhom', title: 'the deputy projects editor' },
+    { name: 'Cameron Fozi', title: 'a projects developer and the projects training manager. He was the spring, summer and fall 2022 projects editor.' },
+    { name: 'Nibras Suliman', title: 'a projects developer. She was a spring 2023 deputy projects editor.' },
+    { name: 'Isabella Borkovic', title: 'a projects developer. She was a spring 2023 deputy projects editor.' },
     { name: 'Tyler Wu', title: 'a projects developer' },
-    { name: 'Lydia Sidhom', title: 'a projects developer' },
     { name: 'Anishi Patel', title: 'a projects developer' },
     { name: 'Rayan Taghizadeh', title: 'a projects developer' },
+    { name: 'Mark Verzhbinsky', title: 'a projects developer' },
+    { name: 'Lauren Lee', title: 'a projects developer' },
+    { name: 'Clara Brownstein', title: 'a projects developer' },
+    { name: 'Emile Shah', title: 'a projects developer' },
+    { name: 'Anika Sikka', title: 'a projects developer' },
     { name: 'Anishi Patel', title: 'a projects developer' },
-    { name: 'Annie Lin', title: 'a projects developer' },
+    { name: 'Riya Chopra', title: 'a projects developer' },
+    { name: 'Claire Roach', title: 'a projects developer' },
   ],
 };
 
