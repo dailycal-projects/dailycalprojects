bd9227f96741998fcedead18dec7f92bc532717d
Cameron Fozi <cameronfozi@berkeley.edu>
2023-11-21 16:59:41 -0800

    rent shelter charts done

--- src/visuals/rent-shelter-visuals/berkeleyAlamedaRent.js
+++ src/visuals/rent-shelter-visuals/berkeleyAlamedaRent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import {
   AreaChart,
   Area,
@@ -11,19 +11,22 @@ import {
 } from 'recharts';
 import { rentShelterData } from './rentShelterData';
 
-const AlignedAxisLabel = ({
-  axisType, x, y, width, height, stroke, children,
-}) => {
-  const isVert = axisType === 'yAxis';
-  const cx = isVert ? x : x + (width / 2);
-  const cy = isVert ? (height / 2) + y : y + height + 10;
-  const rot = isVert ? `270 ${cx} ${cy}` : 0;
-  return (
-    <text x={cx} y={cy} transform={`rotate(${rot})`} textAnchor="middle" stroke={stroke}>
-      {children}
-    </text>
-  );
-};
+class CustomizedAxisTick extends PureComponent {
+  render() {
+    const {
+      x, y, payload,
+    } = this.props;
+
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text x={0} y={0} dy={0} textAnchor="end" fill="#666" transform="rotate(-45)">
+          $
+          {payload.value}
+        </text>
+      </g>
+    );
+  }
+}
 
 const RentChart = () => (
   <div>
@@ -33,12 +36,11 @@ const RentChart = () => (
         justifyContent: 'center',
         alignItems: 'center',
         position: 'relative',
-        left: '20px',
       }}
     >
       <h4> Gross median rent in Berkeley by year </h4>
     </div>
-    <ResponsiveContainer height={400}>
+    <ResponsiveContainer height={450}>
       <AreaChart
         width={750}
         height={400}
@@ -46,9 +48,7 @@ const RentChart = () => (
         syncId="anyI"
         margin={{
           top: 15,
-          right: 15,
-          left: 30,
-          bottom: 30,
+          bottom: 80,
         }}
       >
         <defs>
@@ -58,26 +58,14 @@ const RentChart = () => (
           </linearGradient>
         </defs>
 
-        <XAxis dataKey="year" tick={{ fontSize: 16, transform: 'translate(0, 7)' }}>
+        <XAxis dataKey="year" tick={{ fontSize: 16, transform: 'translate(0, 10)' }}>
           <Label value="Year" offset={-24} position="insideBottom" />
-        </XAxis>
+        </XAxis
+        >
         <YAxis
-          tick={{ fontSize: 16 }}
-          label={(
-            <AlignedAxisLabel
-              x={20}
-              y={160}
-              width={0}
-              height={0}
-              axisType="yAxis"
-            >
-              Gross median rent
-            </AlignedAxisLabel>
-)}
+          tick={<CustomizedAxisTick />}
         />
         <Tooltip separator=": $" />
-        {/* content={<RentCustomTooltip />} */}
-
         <Area
           type="monotone"
           dataKey="Berkeley median rent"
@@ -87,16 +75,12 @@ const RentChart = () => (
       </AreaChart>
     </ResponsiveContainer>
 
-    <br />
-    <br />
-
     <div
       style={{
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         position: 'relative',
-        left: '20px',
       }}
     >
       <h4> Gross median rent in Alameda County by year </h4>
@@ -110,8 +94,6 @@ const RentChart = () => (
           syncId="anyI"
           margin={{
             top: 15,
-            right: 15,
-            left: 30,
             bottom: 30,
           }}
         >
@@ -128,28 +110,16 @@ const RentChart = () => (
             stroke="#b399bc"
             fill="url(#colorRentAlameda)"
           />
-          <XAxis dataKey="year" tick={{ fontSize: 16, transform: 'translate(0, 7)' }}>
+          <XAxis dataKey="year" tick={{ fontSize: 16, transform: 'translate(0, 10)' }}>
             <Label value="Year" offset={-24} position="insideBottom" />
           </XAxis>
           <YAxis
-            tick={{ fontSize: 16 }}
-            label={(
-              <AlignedAxisLabel
-                x={20}
-                y={160}
-                width={0}
-                height={0}
-                axisType="yAxis"
-              >
-                Gross median rent
-              </AlignedAxisLabel>
-                  )}
+            tick={<CustomizedAxisTick />}
           />
           <Tooltip separator=": $" />
         </AreaChart>
       </ResponsiveContainer>
     </div>
-
   </div>
 );
 
