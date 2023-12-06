d7c8b708bde6810dda65198d77afacb9219e26b7
Cameron Fozi <cameronfozi@berkeley.edu>
2023-12-05 22:21:16 -0800

    Commented out faulty imports

--- src/visuals/theft-visuals/bikeTheftMap.js
+++ src/visuals/theft-visuals/bikeTheftMap.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import {
   MapContainer, TileLayer, Popup, Marker, // Map is outdated; Leaflet now uses MapContainer
 } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import 'leaflet.markercluster/dist/MarkerCluster.css';
-import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+// import 'leaflet/dist/leaflet.css';
+// import 'leaflet.markercluster/dist/MarkerCluster.css';
+// import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import {
   InputLabel,
   FormControl,
