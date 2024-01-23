120f65fb39f5cc064e25e555fde8401a522af929
Anika Sikka <anikasikka1@gmail.com>
2024-01-22 22:49:12 -0800

    finished audit visual

--- src/visuals/internal-audit-visuals/internalAuditData.js
+++ src/visuals/internal-audit-visuals/internalAuditData.js
@@ -48,18 +48,303 @@ const age = [
   },
 ];
 
+const year = [
+  {
+    name: 'First Year',
+    Percent: 20.7,
+  },
+  {
+    name: 'Second Year',
+    Percent: 31.5,
+  },
+  {
+    name: 'Third Year',
+    Percent: 26.1,
+  },
+  {
+    name: 'Fourth Year',
+    Percent: 18.9,
+  },
+  {
+    name: 'Fifth Year+',
+    Percent: 1.8,
+  },
+  {
+    name: 'Prefer not to disclose',
+    Percent: 0.9,
+  },
+];
+
+const transfer = [
+  {
+    name: 'Yes',
+    Percent: 11.3,
+  },
+  {
+    name: 'No',
+    Percent: 88.3,
+  },
+  {
+    name: 'Prefer not to disclose',
+    Percent: 0.5,
+  },
+];
+
+const international = [
+  {
+    name: 'Yes',
+    Percent: 6.8,
+  },
+  {
+    name: 'No',
+    Percent: 92.8,
+  },
+  {
+    name: 'Prefer not to disclose',
+    Percent: 0.5,
+  },
+];
+
+const gender = [
+  {
+    name: 'Cisgender woman',
+    Percent: 63.1,
+  },
+  {
+    name: 'Cisgender man',
+    Percent: 27.9,
+  },
+  {
+    name: 'Nonbinary or gender non-conforming, Transgender man',
+    Percent: 0.5,
+  },
+  {
+    name: 'Prefer not to disclose',
+    Percent: 0.9,
+  },
+  {
+    name: 'Nonbinary or gender non-conforming, Unsure',
+    Percent: 0.5,
+  },
+  {
+    name: 'Nonbinary or gender non-conforming',
+    Percent: 1.8,
+  },
+  {
+    name: 'Cisgender woman, Nonbinary or gender non-conforming, Unsure',
+    Percent: 0.9,
+  },
+  {
+    name: 'Unsure',
+    Percent: 0.9,
+  },
+  {
+    name: 'Genderqueer',
+    Percent: 1.8,
+  },
+  {
+    name: 'Cisgender woman, Nonbinary or gender non-conforming',
+    Percent: 0.9,
+  },
+  {
+    name: 'Nonbinary or gender non-conforming, Agender',
+    Percent: 0.5,
+  },
+  {
+    name: 'Genderfluid',
+    Percent: 0.5,
+  },
+];
+
+const lgbtq = [
+  {
+    name: 'Yes',
+    Percent: 35.1,
+  },
+  {
+    name: 'No',
+    Percent: 57.2,
+  },
+  {
+    name: 'Prefer not to disclose',
+    Percent: 4.1,
+  },
+  {
+    name: 'Unsure / questioning',
+    Percent: 3.6,
+  },
+];
+
+const sexuality = [
+  {
+    name: 'Heterosexual',
+    Percent: 58.1,
+  },
+  {
+    name: 'Prefer not to disclose',
+    Percent: 5,
+  },
+  {
+    name: 'Queer',
+    Percent: 5.9,
+  },
+  {
+    name: 'Bisexual',
+    Percent: 12.6,
+  },
+  {
+    name: 'Lesbian',
+    Percent: 2.7,
+  },
+  {
+    name: 'Bisexual, Queer',
+    Percent: 2.3,
+  },
+  {
+    name: 'Asexual',
+    Percent: 1.4,
+  },
+  {
+    name: 'Unsure / Questioning',
+    Percent: 1.8,
+  },
+  {
+    name: 'Gay / Bisexual',
+    Percent: 0.5,
+  },
+  {
+    name: 'Bisexual, Sexually fluid',
+    Percent: 0.9,
+  },
+  {
+    name: 'Pansexual',
+    Percent: 1.4,
+  },
+  {
+    name: 'Heterosexual, Unsure / Questioning',
+    Percent: 0.9,
+  },
+  {
+    name: 'Gay',
+    Percent: 5.9,
+  },
+  {
+    name: 'Gay, Queer',
+    Percent: 0.5,
+  },
+  {
+    name: 'Sexually fluid',
+    Percent: 0.5,
+  },
+];
+
+const race = [
+  {
+    name: 'White',
+    Percent: 26.1,
+  },
+
+  {
+    name: 'Asian / Asian American',
+    Percent: 53.6,
+  },
+
+  {
+    name: 'Latine / Hispanic / Chicanx',
+    Percent: 14.9,
+  },
+  {
+    name: 'Indigenous / Native American / Alaska Native',
+    Percent: 0.9,
+  },
+  {
+    name: 'North African',
+    Percent: 0.5,
+  },
+  {
+    name: 'Middle Eastern',
+    Percent: 1.8,
+  },
+  {
+    name: 'Prefer not to disclose',
+    Percent: 1.4,
+  },
+  {
+    name: 'Black / African American',
+    Percent: 0.9,
+  },
+];
+
+const disability = [
+  {
+    name: 'Yes',
+    Percent: 14.4,
+  },
+  {
+    name: 'No',
+    Percent: 82,
+  },
+  {
+    name: 'Prefer not to disclose',
+    Percent: 3.6,
+  },
+];
+
+const semesters = [
+  {
+    name: '1',
+    Percent: 27.9,
+  },
+  {
+    name: '2',
+    Percent: 16.7,
+  },
+  {
+    name: '3',
+    Percent: 19.4,
+  },
+  {
+    name: '4',
+    Percent: 14.4,
+  },
+  {
+    name: '5',
+    Percent: 82.0,
+  },
+  {
+    name: '6',
+    Percent: 3.6,
+  },
+  {
+    name: '7',
+    Percent: 14.4,
+  },
+  {
+    name: '8',
+    Percent: 82.0,
+  },
+  {
+    name: '9',
+    Percent: 3.6,
+  },
+  {
+    name: '10',
+    Percent: 3.6,
+  },
+];
+
 const accessData = {
   Editor: editorship,
   Age: age,
-  Year: age,
-  'Transfer rate': age,
+  Year: year,
+  'Transfer rate': transfer,
   'International student rate': age,
-  Gender: age,
-  'LQBTQ+ rate': age,
-  Sexuality: age,
-  'Race and ethnicity': age,
-  'Disability rate': age,
-  Semesters: age,
+  Gender: gender,
+  'LQBTQ+ rate': lgbtq,
+  Sexuality: sexuality,
+  'Race and ethnicity': race,
+  'Disability rate': disability,
+  Semesters: semesters,
 };
 
 export default accessData;
