9cb099508016bce560a16ef229c3dd5dfed195b2
Arfa Momin <arfamomin16@gmail.com>
2024-01-28 11:24:29 -0800

    all funding errors fixed

--- src/visuals/construction-visuals/projectsByFundingBar.js
+++ src/visuals/construction-visuals/projectsByFundingBar.js
@@ -7,66 +7,86 @@ import fundData from './data/berk_construction_by_fund.json';
 import './projectsByFundingBar.css';
 
 const measureDescriptions = {
-  'Measure T1': <p>
-    Measure T1 provides $100 million of funding from bond revenue to pay for improvements to
-    <b>City infrastructure and facilities</b>
-    .
-  </p>,
-  'Capital Improvement Fund': <p>
-    The Capital Improvement Fund provides funding for annual street rehabilitations, and most recently, the
-    <b>Southside Complete Streets project</b>
-    .
-  </p>,
-  'State Transportation Tax': <p>
-    A county-level tax applied to shipping within the county's jurisdiction; also used to fund
-    <b>annual street rehabilitations</b>
-    .
-  </p>,
-
-  // "Measure O": <p style={{ color: '#8EC583' }}>Measure O provides $135 million of funding from bond revenue to pay for <b>affordable housing</b> projects.</p>,
-  'Measure O': <p style={{ color: '#215775' }}>
-    Measure O provides $135 million of funding from bond revenue to pay for
-    <b>affordable housing</b>
-    {' '}
-    projects.
-  </p>,
-  'Measure M': <p>
-    Measure M provided $30 million of funding from bond revenue to improve Berkeley’s streets and install
-    <b>green infrastructure</b>
-    {' '}
-    to protect water quality in the Bay.
-  </p>,
-  'Measure F': <p>
-    Starting in FY 2015, Measure F provided an additional 17% of annual funding to the Parks Tax, a parcel tax that pays for
-    <b>park maintenance.</b>
-  </p>,
-  'Measure B & BB': <p>
-    Measure B was approved in 2000, and Measure BB in 2014 to provide sales tax funding for
-    <b>transportation projects</b>
-    .
-  </p>,
-  'Sanitary Sewer Fund': <p>
-    The Sanitary Sewer Fund provides funds for rehabilitating
-    <b>sewage systems</b>
-    .
-  </p>,
-  'Marina Fund': <p>
-    The Marina Fund provides $15 million of funding for repairing docks and dredging in
-    <b>Berkeley's Marina</b>
-    .
-  </p>,
-  'Vehicle Registration Fee (VRF)': <p>
-    {' '}
-    A 0.65% fee applied to the purchase price/value of vehicle; also used to fund
-    <b>annual street rehabilitations</b>
-    .
-  </p>,
-  'Senate Bill 1': <p>
-    Also known as the Road Repair and Accountability Act, this invests $5.4 billion annually to fix
-    <b>roads, freeways and bridges</b>
-    {' '}
-    in communities across California.
-  </p>,
+  'Measure T1': (
+    <p>
+      Measure T1 provides $100 million of funding from bond revenue to pay for improvements to
+      <b>City infrastructure and facilities</b>
+      .
+    </p>
+  ),
+  'Capital Improvement Fund': (
+    <p>
+      The Capital Improvement Fund provides funding for annual street rehabilitations, and most recently, the
+      <b>Southside Complete Streets project</b>
+      .
+    </p>
+  ),
+  'State Transportation Tax': (
+    <p>
+      A county-level tax applied to shipping within the county&apos;s jurisdiction; also used to fund
+      <b>annual street rehabilitations</b>
+      .
+    </p>
+  ),
+  'Measure O': (
+    <p style={{ color: '#215775' }}>
+      Measure O provides $135 million of funding from bond revenue to pay for
+      <b>affordable housing</b>
+      {' '}
+      projects.
+    </p>
+  ),
+  'Measure M': (
+    <p>
+      Measure M provided $30 million of funding from bond revenue to improve Berkeley’s streets and install
+      <b>green infrastructure</b>
+      {' '}
+      to protect water quality in the Bay.
+    </p>
+  ),
+  'Measure F': (
+    <p>
+      Starting in FY 2015, Measure F provided an additional 17% of annual funding to the Parks Tax, a parcel tax that pays for
+      <b>park maintenance.</b>
+    </p>
+  ),
+  'Measure B & BB': (
+    <p>
+      Measure B was approved in 2000, and Measure BB in 2014 to provide sales tax funding for
+      <b>transportation projects</b>
+      .
+    </p>
+  ),
+  'Sanitary Sewer Fund': (
+    <p>
+      The Sanitary Sewer Fund provides funds for rehabilitating
+      <b>sewage systems</b>
+      .
+    </p>
+  ),
+  'Marina Fund': (
+    <p>
+      The Marina Fund provides $15 million of funding for repairing docks and dredging in
+      <b>Berkeley&apos;s Marina</b>
+      .
+    </p>
+  ),
+  'Vehicle Registration Fee (VRF)': (
+    <p>
+      {' '}
+      A 0.65% fee applied to the purchase price/value of the vehicle; also used to fund
+      <b>annual street rehabilitations</b>
+      .
+    </p>
+  ),
+  'Senate Bill 1': (
+    <p>
+      Also known as the Road Repair and Accountability Act, this invests $5.4 billion annually to fix
+      <b>roads, freeways and bridges</b>
+      {' '}
+      in communities across California.
+    </p>
+  ),
 };
 
 export default function ProjectsByFundRe() {
@@ -197,22 +217,21 @@ export default function ProjectsByFundRe() {
             <Tooltip cursor={false} />
             {/* <Legend /> */}
             <Bar dataKey="count" fill="#BABABA">
-              {fundData.map((entry, index) =>
-              // console.log(index, entry);
-                (
-                  <Cell
-                    cursor="pointer"
-                    fill={entry['Funding Source'] === 'Measure O' ? '#215775' : '#BABABA'}
+              {fundData.map((entry) => (
+                // console.log(index, entry);
+                <Cell
+                  cursor="pointer"
+                  fill={entry['Funding Source'] === 'Measure O' ? '#215775' : '#BABABA'}
                   // key={`cell-${index}`}
-                    onClick={() => {
-                      if (Object.keys(measureDescriptions).includes(entry['Funding Source'])) {
-                        setCurrentMeasure(entry['Funding Source']);
-                      } else {
-                        setCurrentMeasure('');
-                      }
-                    }}
-                  />
-                ))}
+                  onClick={() => {
+                    if (Object.keys(measureDescriptions).includes(entry['Funding Source'])) {
+                      setCurrentMeasure(entry['Funding Source']);
+                    } else {
+                      setCurrentMeasure('');
+                    }
+                  }}
+                />
+              ))}
             </Bar>
             {/* <Brush dataKey="count" height={30} stroke="#BABABA" /> */}
           </BarChart>
