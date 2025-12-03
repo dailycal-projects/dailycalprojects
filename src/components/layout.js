7766de7df334d4ea06f9d660c5e2f4315891e1cc
lucavici2 <luca@vicisano.net>
2025-12-03 11:23:48 -0800

    [Feat] Athletes Added

--- src/components/layout.js
+++ src/components/layout.js
@@ -68,6 +68,8 @@ import ClassDayMap from '../visuals/campus-busyness-visuals/visuals/ClassDayMap'
 import ClassEnrollmentLineChart from '../visuals/campus-busyness-visuals/visuals/ClassEnrollmentLineChart';
 import ClassSizeBarChart from '../visuals/campus-busyness-visuals/visuals/ClassSizeBarChart';
 
+import DatawrapperChart from './dataWrapper';
+
 /*
 To avoid using exact paths in MDX files, import your components here
 and add them to the shortcodes list to be globally accessible.ß
@@ -147,6 +149,7 @@ const shortcodes = {
   ClassDayMap,
   ClassEnrollmentLineChart,
   ClassSizeBarChart,
+  DatawrapperChart,
 };
 
 export default function Layout({ children }) {
