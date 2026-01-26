91913297276d9f3dbd70bb6e4bee27ee52b450f5
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2026-01-25 18:12:47 -0800

    this should build correctly

--- src/visuals/too-sexy-visuals/icon.js
+++ src/visuals/too-sexy-visuals/icon.js
@@ -1,11 +1,11 @@
 import L from 'leaflet';
 
-const sex_spot_icon_2018 = new L.icon({
+const sex_spot_icon_2018 = L.icon({
   iconUrl: '/leaflet/icons/2-sexy-2018-icon.png',
   iconSize: [16, 16],
 });
 
-const sex_spot_icon_2026 = new L.icon({
+const sex_spot_icon_2026 = L.icon({
   iconUrl: '/leaflet/icons/2-sexy-2026-icon.png',
   shadowUrl: '/leaflet/icons/2-sexy-2026-shadow.png',
   shadowSize: [32, 32],
